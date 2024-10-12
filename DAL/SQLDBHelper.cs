d47cd84de699c2f132f1a46e98824cf800ffcd85
JuandaMonkey <juandanieldecide@gmail.com>
2024-10-12 01:35:58 -0600

    DAL - conectividad a base de datos

---
+++ DAL/SQLDBHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.Remoting;
+
+namespace DAL
+{
+    public class SQLDBHelper
+    {
+
+        DataTable tabla;
+        SqlConnection strConexion = new SqlConnection("Data Source=JUANDA\\SQLEXPRESS;Initial Catalog=BDUbicaciones;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+
+        public bool ejecutarComandoSQL(SqlCommand strSQLCommand)
+        {
+
+            bool Respuesta = true;
+
+            cmd = strSQLCommand;
+            cmd.Connection = strConexion;
+            strConexion.Open();
+            Respuesta = (cmd.ExecuteNonQuery() <= 0) ? false : true;
+            strConexion.Close();
+
+            return Respuesta;
+
+        }
+
+        public DataTable ejecutarSentenciaSQL(SqlCommand strSQLCommand)
+        {
+
+            cmd = strSQLCommand;
+            cmd.Connection = strConexion;
+            strConexion.Open();
+            tabla = new DataTable();
+            tabla.Load(cmd.ExecuteReader());
+            strConexion.Close();
+
+            return tabla;
+
+        }
+
+    }
+
+}
---
+++ DAL/ubicaciones_DAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+
+namespace DAL
+{
+
+    public class ubicaciones_DAL
+    {
+
+        SQLDBHelper oConexion;
+
+        public ubicaciones_DAL()
+        {
+
+            oConexion = new SQLDBHelper();
+
+        }
+
+        public void agregar() { }
+        public void eliminar() { }
+        public void modificar() { }
+        public DataTable listar()
+        {
+
+            SqlCommand cmdComando = new SqlCommand();
+            cmdComando.CommandText = "select * from direcciones";
+            cmdComando.CommandType = CommandType.Text;
+
+            DataTable tablaResultado = oConexion.ejecutarSentenciaSQL(cmdComando);
+
+            return tablaResultado;
+
+        }
+    }
+
+}
