cd922ad6c897363fd6828ced0856befecc7294e0
JuandaMonkey <juandanieldecide@gmail.com>
2024-10-12 01:35:15 -0600

    BLL - modelo creado

---
+++ BLL/ubicaciones_BLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+
+    public class ubicaciones_BLL
+    {
+
+        public int id { get; set; }
+        public string ubicacion { get; set; }
+        public string latitud { get; set; }
+        public string longitud { get; set; }
+
+    }
+
+}
